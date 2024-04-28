c1f7682d23ef34d01ca866113771387598ac20e2
shaobeichen <shaobeichen@outlook.com>
2024-04-28 22:16:27 +0800

    feat: add canvas test component

--- packages/moten-admin/src/pages/test/index.vue
+++ packages/moten-admin/src/pages/test/index.vue
@@ -1,60 +1,55 @@
 <template>
-  <div class="flex jc">
-    <drag :list="newList" :sort="false" :group="{ name: 'people', pull: 'clone', put: false }" />
-    <drag type="render" :list="list" group="people" style="width: 500px; background: #ddd" />
-    <pre v-html="JSON.stringify(list, null, 3)" style="font-size: 8px"></pre>
-    <de-image
-      src="https://raw.githubusercontent.com/SortableJS/Vue.Draggable/master/logo.svg?sanitize=true"
-      style="width: 100px; height: 100px"
-    />
+  <div class="page">
+    <div class="dropzone">
+      <div id="draggable" draggable="true">This div is draggable</div>
+    </div>
+    <div
+      class="dropzone"
+      id="droptarget"
+      @dragenter="dragenter"
+      @dragleave="dragend"
+      @dragover.prevent
+      @drop.prevent="drop"
+    ></div>
   </div>
 </template>
 
-<script lang="ts">
-import drag from './drag.vue'
-
-export default {
-  components: {
-    drag,
-  },
-  data() {
-    return {
-      newList: [
-        {
-          name: '多列',
-          code: 'column',
-          nested: true,
-          children: [[], [], []],
-        },
-        {
-          name: '图片',
-          code: 'image',
-        },
-        {
-          name: '视频',
-          code: 'video',
-        },
-        {
-          name: '按钮',
-          code: 'button',
-        },
-        {
-          name: '文本',
-          code: 'text',
-        },
-        {
-          name: '幻灯片',
-          code: 'swiper',
-        },
-        {
-          name: '留白',
-          code: 'blank',
-        },
-      ],
-      list: [],
-    }
-  },
+<script setup>
+const dragenter = (event) => {
+  if (event.target.classList.contains('dropzone')) {
+    event.target.classList.add('dragover')
+  }
+}
+const dragend = (event) => {
+  if (event.target.classList.contains('dropzone')) {
+    event.target.classList.remove('dragover')
+  }
+}
+const drop = (event) => {
+  const { offsetX, offsetY } = event
+  console.warn(event, offsetX, offsetY)
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.page {
+  padding-left: 100px;
+  padding-top: 100px;
+}
+#draggable {
+  text-align: center;
+  background: white;
+}
+
+.dropzone {
+  width: 200px;
+  height: 200px;
+  background: blueviolet;
+  margin: 10px;
+  padding: 10px;
+}
+
+.dropzone.dragover {
+  background-color: purple;
+}
+</style>
