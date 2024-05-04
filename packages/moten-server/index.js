a588c8edb23c7923f88bf7c1aa26dc041e2b7947
shaobeichen <shaobeichen@outlook.com>
2024-05-04 16:30:44 +0800

    feat(server): handle cors

--- packages/moten-server/index.js
+++ packages/moten-server/index.js
@@ -1,8 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import controller from './controller/index.js'
 import error from './middleware/error.js'
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
