37f385643fe2fe11800ce11e8c3f1638bda2ef2a
shaobeichen <shaobeichen@outlook.com>
2024-05-16 11:10:54 +0800

    feat(admin): update logo icon

--- packages/moten-admin/components.d.ts
+++ packages/moten-admin/components.d.ts
@@ -33,7 +33,6 @@ declare module 'vue' {
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElInput: typeof import('element-plus/es')['ElInput']
-    ElInputNumber: typeof import('element-plus/es')['ElInputNumber']
     ElOption: typeof import('element-plus/es')['ElOption']
     ElSelect: typeof import('element-plus/es')['ElSelect']
     ElTooltip: typeof import('element-plus/es')['ElTooltip']
