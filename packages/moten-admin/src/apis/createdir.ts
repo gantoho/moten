4f6428f17101c07891557908f42c28cc6bdb1bc8
shaobeichen <shaobeichen@outlook.com>
2024-05-17 11:20:35 +0800

    chore: clear unused code

--- packages/moten-admin/src/apis/createdir.ts
+++
@@ -1,7 +0,0 @@
-export const url = 'https://test-backend.celine.cn/admin_142upl/page_design/file/createdir'
-
-export default {
-  name: 'aaa/111/',
-  isAjax: true,
-  form_key: 'T4jSX8K0JBECz7Ic',
-}
--- packages/moten-admin/src/apis/deletefile.ts
+++
@@ -1,7 +0,0 @@
-export const url = 'https://test-backend.celine.cn/admin_142upl/page_design/file/deletefile'
-
-export default {
-  name: 'absa/sss1.jpg',
-  isAjax: true,
-  form_key: 'T4jSX8K0JBECz7Ic',
-}
