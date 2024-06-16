691629fc78b91616c40fbf92efe62f83b4409797
shaobeichen <shaobeichen@outlook.com>
2024-06-16 20:45:11 +0800

    feat: 服务端 预览访问 代码热更新 跨域

--- packages/moten-server/index.js
+++ packages/moten-server/index.js
@@ -1,9 +1,20 @@
 import express from 'express'
+import cors from 'cors'
 const app = express()
 const port = 3000
 
+app.use(cors())
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/moten', (req, res) => {
+  res.send('Hello moten!')
+})
+
+app.get('/1111', (req, res) => {
+  res.send('Hello 1111!')
 })
 
 app.listen(port, () => {
